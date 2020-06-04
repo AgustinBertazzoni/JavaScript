c7cc14c550a5c54517485a63f65f3fe52a1150d8
AgustinBertazzoni <agustin.bertazzoni@hotmail.com>
2020-06-04 20:42:49 -0300

    Agregando ficha + activando boton de atras y adelante en el navegador

--- docs/producto.class.js
+++ docs/producto.class.js
@@ -1,6 +1,7 @@
 class Producto {
 
-    constructor(n, s, p, i, m) {
+    constructor(id, n, s, p, i, m) {
+        this.id = id
         this.nombre = n
         this.stock = s
         this.precio = p
@@ -21,8 +22,8 @@ class Producto {
                             <div class="card-body">
                                 <h4 class="card-title"><a href="#">${this.marca} - ${this.nombre}</a> <span class="badge badge-pill badge-success float-right">$${parseFloat(this.precio).toFixed(2)}</span></h4>
                                 <p class="card-text">${this.stock} unid.</p>
-                                <button class="btn btn-warning btn-editar float-left ${(auth2 && auth2.isSignedIn.get() == true) ? "d-block" : "d-none"}">Editar</button>
-                                <button class="btn btn-primary btn-comprar float-right">Comprar</button>
+                                <button class="btn btn-warning btn-editar float-left ${ (auth2 && auth2.isSignedIn.get() == true) ? "d-block" : "d-none"}">Editar</button>
+                                <button class="btn btn-primary btn-ver float-right">Ver +</button>
                             </div>
                         </div>`
         //debugger
@@ -34,22 +35,21 @@ class Producto {
         this.vDOM.querySelector(".btn-editar").onclick = (evento) => {
             //console.log(evento.target) //<-- El boton clickeado
 
-            if (auth2.isSignedIn.get()) {    // Si estoy logueado
+            console.log("Esta logeado?")
+            console.log(auth2.isSignedIn.get())
 
-
-                // Editar Producto
+            if (auth2.isSignedIn.get()) { //<-- Si estoy logeado...
+                //1) Editar el producto...
                 this.marca = prompt("Ingrese nueva marca:", this.marca)
                 this.nombre = prompt("Ingrese nuevo nombre:", this.nombre)
                 this.stock = prompt("Ingrese nuevo stock:", this.stock)
                 this.precio = prompt("Ingrse nuevo precio:", this.precio)
                 this.imagen = prompt("Ingrese nueva imagen:", this.imagen)
 
-
-                //Volver a mostrar la interfaz
+                //2) Re-renderizar la interfaz...
                 this.Mostrar()
 
-
-                //Enviar los nuevos datos al servidor...
+                //3) Enviar los nuevos datos al servidor...
                 let datos = new FormData()
                 datos.append("marca", this.marca)
                 datos.append("nombre", this.nombre)
@@ -68,12 +68,28 @@ class Producto {
 
                 console.log(this) //<-- El objeto "Producto" con el que se armó la interfaz
             } else {
-                alert("Acceso denegado")
+                alert("¡¡ACCESO DENEGADO!!")
             }
-
 
         }
 
+        this.vDOM.querySelector(".btn-ver").onclick = (evento) => {
+
+            //Ej: /mt-1-apple-iphone-6
+            const path = (`/MT-${this.id}-${this.marca.replace(" ", "-")}-${this.nombre.split(" ").join("-")}`).toLowerCase()
+
+            const HTML = document.querySelector("#producto")
+
+            HTML.querySelector("#producto-imagen").src = this.imagen
+            HTML.querySelector("#producto-nombre").innerText = this.marca + " " + this.nombre
+            HTML.querySelector("#producto-precio").innerText = "$" + this.precio
+            HTML.querySelector("#producto-detalle").innerText = "Lorem impsum dolor sit ammet..."
+            HTML.classList.remove("d-none")
+
+            document.querySelector("#productos-destacados").classList.add("d-none")
+
+            window.history.pushState({}, path, window.location.origin + path)
+        }
 
     }
 
@@ -85,11 +101,10 @@ class Producto {
     //////////////////////////////////////////////////////////////////
     static armarCatalogo(objetos, rango) { //<-- Metodos de Clase (o estaticos)
 
-        let productos = objetos.map(({ Nombre, Stock, Precio, Imagen, Marca }) => new Producto(Nombre, Stock, Precio, Imagen, Marca))
+        let productos = objetos.map(({ idProducto, Nombre, Stock, Precio, Imagen, Marca }) => new Producto(idProducto, Nombre, Stock, Precio, Imagen, Marca))
 
         let resultado = rango ? productos.filter(producto => producto.precio > rango.min && producto.precio < rango.max) : productos  //<-- Operador ternario
 
         return resultado
     }
 }
-
